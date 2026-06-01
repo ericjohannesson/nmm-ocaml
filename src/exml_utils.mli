358608d703ba05573a1f72ffc02b2662fdd734ac
eric <eric@ericjohannesson.com>
2026-06-01 18:03:25 +0200

    documentation

--- src/exml_utils.mli
+++ src/exml_utils.mli
@@ -41,3 +41,8 @@ val attr_list_of_tr_id_opt : Common_utils.t_doc_settings -> Common_utils.t_path 
 val par_hdr_opt: Common_utils.t_doc_settings -> Common_utils.t_cref_table -> Common_utils.t_nte_table -> Common_utils.t_path -> Doc_types.tu_tag_or_id option -> Doc_types.ts_hdr option -> (Xml.xml list) option
 
 val normalize_exml : Xml.xml -> Xml.xml
+
+val exml_schema : unit -> string
+(**
+[exml_schema ()] evaluates to a string-representation of {{:specs/exml.dtd.txt}exml.dtd}.
+*)
