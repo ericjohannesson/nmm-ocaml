bd0d9d10ec9263b0dabd76c8fed93d7b46a4e321
eric <eric@ericjohannesson.com>
2026-09-03 04:13:58 +0200

    added ISO language codes to bash completion script

--- scripts/nmm-ocaml-bash-completion.sh
+++ scripts/nmm-ocaml-bash-completion.sh
@@ -12,7 +12,7 @@ _nmm_ocaml_get_options () {
 	local txt_options="$exml_options --margin --indent --width"
 	local html_options="$exml_options --margin --indent --internal-css --external-css --lang"
 	local numbering_options='a1i ai1 1ai 1ia ia1 i1a'
-	local lang_options='en sv fr de es' #etc
+	local lang_options='ab aa af ak sq am ar an hy as av ae ay az bm ba eu be bn bh bi bs br bg my ca ch ce ny zh zh-Hans zh-Hant cv kw co cr hr cs da dv nl dz en eo et ee fo fj fi fr ff gl gd gv ka de el kl gn gu ht ha he hz hi ho hu is io ig id in ia ie iu ik ga it ja jv kl kn kr ks kk km ki rw rn ky kv kg ko ku kj lo la lv li ln lt lu lg lb gv mk mg ms ml mt mi mr mh mo mn na nv ng nd ne no nb nn ii oc oj cu or om os pi ps fa pl pt pa qu rm ro ru se sm sg sa sr sh st tn sn ii sd si ss sk sl so nr es su sw ss sv tl ty tg ta tt te th bo ti to ts tr tk tw ug uk ur uz ve vi vo wa cy wo fy xh yi ji yo za zu'
 
 	case $1 in
 		nmm-ocaml )
@@ -64,8 +64,11 @@ _nmm_ocaml () {
 			_nmm_ocaml_chosen_subcommand=$prev
 			COMPREPLY=( $(compgen -W "${options}" -- ${cur}) $(compgen -f -- ${cur}) )
 			;;
-		--numbering | --margin | --indent | --width | --lang )
+		--lang )
 			COMPREPLY=( $(compgen -W "${options}" -- ${cur}) )
+			;;
+		--numbering | --margin | --indent | --width )
+			COMPREPLY=( $(compgen -W "${options}") )
 			;;
 		--tags | --internal-css | --external-css | --validate-xml | --check-xml-schema )
 			COMPREPLY=( $(compgen -f -- ${cur}) )
